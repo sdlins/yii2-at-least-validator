7db09c54a10bee1881f38b03ee66b791e9b45d11
Sidney Lins (slinstj) <slinstj@gmail.com>
2016-05-12 00:18:31 -0300

    New implementation

--- ChooseValidator.php
+++ ChooseValidator.php
@@ -6,35 +6,32 @@ use Yii;
 use yii\base\InvalidConfigException;
 
 /**
- * Checks if one or more of the choosen attributes are filled.
+ * Checks if one or more in a list of attributes are filled.
  *
- * In the following example, the `attr1`, `attr2` and `attr3` attributes will
+ * In the following example, the `attr1` and `attr2` attributes will
  * be verified. If none of them are filled they will receive an error:
  *
  * ~~~[php]
  *      // in rules()
  *      return [
- *          [['attr1', 'attr2, 'attr3'], 'choose'],
+ *          [['attr1', 'attr2'], AtLeastValidator::className()],
  *      ];
  * ~~~
  *
  * In the following example, the `attr1`, `attr2` and `attr3` attributes will
- * be verified. If at least 2 (`min`) of them are not filled, `attr1` and `attr3` will
+ * be verified. If at least 2 (`min`) of them are not filled, `attr1` will
  * receive an error:
  *
  * ~~~[php]
  *      // in rules()
  *      return [
- *          [['attr1', 'attr2', 'attr3'], 'choose', 'min' => 2, 'errorIn' => 'attr1'],
+ *          ['attr1', AtLeastValidator::className(), 'min' => 2, 'in' => ['attr1', 'attr2', 'attr3']],
  *      ];
  * ~~~
  *
- * The attributes that will receive the error message can be specified into [[errorIn]]
- * param. If none is specified, all envolved attributes will receive the error.
- *
  * @author Sidney Lins <slinstj@gmail.com>
  */
-class ChooseValidator extends Validator
+class AtLeastValidator extends Validator
 {
     /**
      * @var integer the minimun required quantity of attributes that must to be filled.
@@ -43,10 +40,10 @@ class ChooseValidator extends Validator
     public $min = 1;
 
     /**
-     * @var array the list of attributes that should receive the error message.
-     * Defaults to all attributes being validated (i.e, attribute + [[with]] attributes).
+     * @var string|array the list of attributes that should receive the error message.
+     * Defaults to all attributes being validated.
      */
-    public $errorIn;
+    public $in;
 
     /**
      * @var boolean whether this validation rule should be skipped if the attribute value
@@ -61,30 +58,27 @@ class ChooseValidator extends Validator
     {
         parent::init();
         if ($this->message === null) {
-            $this->message = Yii::t('yii', 'Please, fill at least {min} of the attributes {attributesList}.');
+            $this->message = 'At least {min} of the attributes {attributes}';
         }
     }
 
     public function validateAttribute($model, $attribute)
     {
-        $value = $model->$attribute;
+        $attributes = (array) $this->in ?? (array) $attribute;
 
-        $attributes = array_merge((array) $attribute, (array) $this->with);
-        $filled = 0;
-        foreach ($attributes as $attribute) {
-            $value = $model->$attribute;
-            $attributesListLabels[] = '"' . $model->generateAttributeLabel($attribute) . '"';
-            $filled += !empty($value) ? 1 : 0;
+        foreach ($attributes as $attributeName) {
+            $value = $model->$attributeName;
+            $attributesListLabels[] = '"' . $model->generateAttributeLabel($attributeName) . '"';
+            $choosen += !empty($value) ? 1 : 0;
         }
 
-        if (!$filled) {
-            $attributesList = implode(' or ', $attributesListLabels);
-            $errorIn = !empty($this->errorIn) ? (array) $this->errorIn : $attributes;
-            foreach ($errorIn as $attribute) {
-                $this->addError($model, $attribute, $this->message, [
-                    'attributesList' => $attributesList,
-                ]);
-            }
+        if (!$choosen) {
+            $attributesList = implode(', ', $attributesListLabels);
+            $message = strtr($this->message, [
+                '{min}' => $this->min,
+                '{attributes}' => $attributesList,
+            ]);
+            $model->addError($attribute, $message);
         }
     }
 }
