098d5a1d7d1eb06750779186ef9e67c6796a2f9d
vagrant <yassin.mumen@gmail.com>
2017-07-27 10:23:46 +0000

    Fixed build failing.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,8 @@
 $vendor = __DIR__ . '/../vendor';
 require_once ($vendor . '/autoload.php');
 require_once ($vendor . '/yiisoft/yii2/Yii.php');
+
+use yii\console\Application;
+
+// Initializes a Yii2 console application and assigns it to Yii::$app.
+new Application(['id' => 'testing-app', 'basePath' => dirname(__DIR__)]);
