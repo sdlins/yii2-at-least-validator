8f91a7243d2acf5a05dd973ce8e9349aad8632aa
Вячеслав Горинь <gorin@list.local>
2018-06-01 08:04:57 +0500

    russian messages

---
+++ messages/ru-RU/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'You must fill at least {min} of the attributes {attributes}.' => 'Вы должны заполнить хотя бы {min} из следующих полей: {attributes}.',
+    ' or ' => ' или ',
+];
+
